6898c29651e47ba27205063ec4ebbc886808aedc
Julien Kauffmann <julien.kauffmann@freelan.org>
2020-03-14 12:26:53 -0400

    Reorganised files and classes

--- led_stripe.py
+++ led_stripe.py
@@ -5,15 +5,6 @@ import math
 
 from urllib.parse import urljoin
 
-
-class StateOutdated(RuntimeError):
-    """
-    Indicates that a state update failed because the revision number was too old.
-    """
-    def __init__(self, state):
-        self.state = state
-
-
 class LEDStripe:
     """
     Represents and control an Ohm-LED device.
