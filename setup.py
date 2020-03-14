d0da4e1e9a7ba5d874a1d3622ee56713940693bf
Julien Kauffmann <julien.kauffmann@freelan.org>
2020-03-14 15:04:44 -0400

    Fixed python package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ohm-made",
-    version="0.0.1",
+    version="0.0.2",
     author="Julien Kauffmann",
     author_email="julien.kauffmann@freelan.org",
     description="A library to control Ohm-Made devices.",
